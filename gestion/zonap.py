fce49d06d6b51ad817969dc01f1041c817d64667
LuisM11 <lumarinb@unal.edu.co>
2021-11-22 10:34:44 -0500

    zonaself

--- gestion/zonap.py
+++ gestion/zonap.py
@@ -1,15 +1,16 @@
 #from zooAnimales.animalp import Animal
 class Zona:
-    _animales=[]
+    
     def __init__(self, n=None,z=None,):
         self._nombre=n
         self._zoo=z
+        self._animales=[]
         
     def agregarAnimales(self,a):
-        Zona._animales.append(a)
+        self._animales.append(a)
 
     def cantidadAnimales(self):
-        return len(Zona._animales)
+        return len(self._animales)
     def getNombre(self):
         return self._nombre
     def getZoo(self):
